2e9128ea8268012060200aff5c31292f4519cab9
Matt Weeks <m_weeks@hotmail.co.uk>
2015-12-27 02:29:02 +0000

    Add new python files

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+	def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_url = movie_poster
+		self.trailer_url = movie_trailer
---
+++ media_center.py
@@ -0,0 +1,7 @@
+import media
+
+home = media.Movie("Home (2015)", "Aliens invade earth...", "http://www.impawards.com/2015/posters/home_ver2_xlg.jpg", "https://www.youtube.com/watch?v=MyqZf8LiWvM")
+#inside_out = media.Movie()
+#jurassic_world = media.Movie()
+
+print(home.storyline);
