39e45d3a9185640b804dd3a24333bca2587373d8
Matt Weeks <m_weeks@hotmail.co.uk>
2015-12-27 03:51:16 +0000

    Add inheritance.

--- media.py
+++ media.py
@@ -1,11 +1,21 @@
 import webbrowser
 
-class Movie():
+class Video():
+    """Docs and stuff..."""
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+    def show_info(self):
+        print("Title: "+self.title})
+
+class Movie(Video):
     """This is my documentation..."""
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_year, movie_storyline, movie_poster, movie_trailer):
-        self.title = movie_title
+    def __init__(self, movie_title, movie_duration, movie_year, movie_storyline, movie_poster, movie_trailer):
+        Video(movie_title, movie_duration)
         self.year = movie_year
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
@@ -13,3 +23,11 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
+
+
+class TvShow(Video):
+    """This is my other documentation..."""
+
+    def __init__(self, tvshow_title, tvshow_duration):
+        Video(tvshow_title, tvshow_duration)
+        self.x = 1
