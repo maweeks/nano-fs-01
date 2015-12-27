ac8c67e51019fc05c86d3521cebe41d0da75e055
Matt Weeks <m_weeks@hotmail.co.uk>
2015-12-27 02:45:48 +0000

    Add second movie.

--- media_center.py
+++ media_center.py
@@ -1,7 +1,8 @@
 import media
 
 home = media.Movie("Home (2015)", "Aliens invade earth...", "http://www.impawards.com/2015/posters/home_ver2_xlg.jpg", "https://www.youtube.com/watch?v=MyqZf8LiWvM")
-#inside_out = media.Movie()
+inside_out = media.Movie("Inside Out (2015)", "A view of the battles inside the mind of a child while moving house...", "[Movie Poster Link]", "[Movie Trailer Link]")
 #jurassic_world = media.Movie()
+#movieX = media.Movie("[Movie Name]", "[Movie Storyline]", "[Movie Poster Link]", "[Movie Trailer Link]")
 
-print(home.storyline);
+print(inside_out.storyline);
